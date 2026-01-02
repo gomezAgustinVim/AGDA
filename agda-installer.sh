3890f767a6601490f7d47465c92054fce7c5be6b
lordagustin22 <niconecrofilo589@gmail.com>
2026-01-02 13:49:12 -0300

    Funcion para instalar distintos programas; login automatico en tty; sudoers file

--- agda-installer.sh
+++ agda-installer.sh
@@ -30,17 +30,10 @@ welcomemsg() {
 # ya sea con el archinstall o manualmente
 
 getuserandpass() {
-	# Prompts user for new username and password.
+    # Consigue el nombre de usuario ya existente
 	name=$(whiptail --inputbox "Nombre de usuario de la cuenta" 10 60 3>&1 1>&2 2>&3 3>&1) || exit 1
 	while ! echo "$name" | grep -q "^[a-z_][a-z0-9_-]*$"; do
 		name=$(whiptail --nocancel --inputbox "Nombre de usuario no valido. Escribe un nombre de usuario que empiece por una letra, con solo letras minúsculas, - o _." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done
-	pass1=$(whiptail --nocancel --passwordbox "Contraseña" 10 60 3>&1 1>&2 2>&3 3>&1)
-	pass2=$(whiptail --nocancel --passwordbox "Reescribre la contraseña" 10 60 3>&1 1>&2 2>&3 3>&1)
-	while ! [ "$pass1" = "$pass2" ]; do
-		unset pass2
-		pass1=$(whiptail --nocancel --passwordbox "Las contraseñas no coinciden.\\n\\nReescribe la contraseña" 10 60 3>&1 1>&2 2>&3 3>&1)
-		pass2=$(whiptail --nocancel --passwordbox "Reescribre la contraseña" 10 60 3>&1 1>&2 2>&3 3>&1)
 	done
 }
 
@@ -52,15 +45,13 @@ usercheck() {
 }
 
 adduserandpass() {
-	# Adds user `$name` with password $pass1.
-	whiptail --infobox "Adding user \"$name\"..." 7 50
-	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
-		usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
+	# Adds user `$name` to wheel
+	whiptail --infobox "Añadiendo user a grupo wheel \"$name\"..." 7 50
+	usermod -a -G wheel "$name" && chown "$name":wheel /home/"$name"
 	export repodir="/home/$name/.local/src"
+	whiptail --infobox "Configurando directorio de repositorios \"$repodir\"..." 7 50
 	mkdir -p "$repodir"
 	chown -R "$name":wheel "$(dirname "$repodir")"
-	echo "$name:$pass1" | chpasswd
-	unset pass1 pass2
 }
 
 # Suponiendo que hubo una instalación de Arch a través del script de instalación de Arch
@@ -72,6 +63,13 @@ installmain() {
     installpkg "$1"
 }
 
+aurinstall() {
+	whiptail --title "Instalación de AGDA" \
+		--infobox "Instalando \`$1\` ($n of $total) del AUR. $1 $2" 9 70
+	echo "$aurinstalled" | grep -q "^$1$" && return 1
+	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
+}
+
 installfonts() {
     # Instalar fuentes
 	whiptail --title "Instalación de AGDA" --infobox "Instalando fuentes ($n de $total). $1 $2" 9 70
@@ -80,7 +78,33 @@ installfonts() {
 
 getdotfiles() {
     git clone $dotsfilesrepo
-    mv duandotfiles/.config ~
+    mv duandotfiles/.* ~/
+}
+
+installationloop() {
+    # Loop to install all packages in progs.csv
+	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) ||
+		curl -Ls "$progsfile" | sed '/^#/d' >/tmp/progs.csv
+	total=$(wc -l </tmp/progs.csv)
+	aurinstalled=$(pacman -Qqm)
+	while IFS=, read -r tag program; do
+		n=$((n + 1))
+		case "$tag" in
+		    "A") aurinstall "$program" "$comment" ;;
+		    "G") gitmakeinstall "$program" "$comment" ;;
+		    *) maininstall "$program" "$comment" ;;
+        esac
+    done </tmp/progs.csv
+}
+
+setautomaticlogin() {
+    # Set automatic login
+    whiptail --title "AGDA" --infobox "Configurando login automático" 8 70
+    mkdir -p /etc/systemd/system/getty@tty1.service.d
+    touch /etc/systemd/system/getty@tty1.service.d/autologin.conf
+    echo "[Service]" > /etc/systemd/system/getty@tty1.service.d/autologin.conf
+    echo "ExecStart=" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
+    echo "ExecStart=-/sbin/agetty --noreset --noclear --autologin $name \${TERM}" >> /etc/systemd/system/getty@tty1.service.d/autologin.conf
 }
 
 # Script principal
@@ -95,6 +119,14 @@ whiptail --title "AGDA Instalación" \
 	--infobox "Sincronizando con el servidor de tiempo" 8 70
 ntpd -q -g >/dev/null 2>&1
 
+[ -f /etc/sudoers.pacnew ] && cp /etc/sudoers.pacnew /etc/sudoers # Just in case
+
+# Sudo sin contraseña para el usuario, para que el usuario pueda instalar paquetes
+# en un entorno fakeroot para el AUR
+trap 'rm -f /etc/sudoers.d/agda-temp' HUP INT QUIT TERM PWR EXIT
+echo "%wheel ALL=(ALL) NOPASSWD: ALL
+Defaults:%wheel,root runcwd=*" >/etc/sudoers.d/agda-temp
+
 # Make pacman colorful, concurrent downloads and Pacman eye-candy.
 grep -q "ILoveCandy" /etc/pacman.conf || sed -i "/#VerbosePkgLists/a ILoveCandy" /etc/pacman.conf
 sed -Ei "s/^#(ParallelDownloads).*/\1 = 5/;/^#Color$/s/#//" /etc/pacman.conf
@@ -102,7 +134,10 @@ sed -Ei "s/^#(ParallelDownloads).*/\1 = 5/;/^#Color$/s/#//" /etc/pacman.conf
 # Instalacion de todos los paquetes en progs.csv
 installationloop
 
-# Make zsh the default shell for the user.
+# Hacer zsh el shell por defecto
 chsh -s /bin/zsh "$name" >/dev/null 2>&1
 sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
 sudo -u "$name" mkdir -p "/home/$name/.config/mpd/playlists/"
+
+# Hacer dash el enlace simbólico a sh por defecto
+ln -sfT /bin/dash /bin/sh >/dev/null 2>&1
